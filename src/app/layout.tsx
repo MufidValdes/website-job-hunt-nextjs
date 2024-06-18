e33431be99423921063d467c7d7e21a716e70736
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-06-18 11:44:43 +0700

    landing page + assets

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Epilogue } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
 
-const inter = Inter({ subsets: ["latin"] });
+const epilogue = Epilogue({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -11,12 +12,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${epilogue.className} relative overflow-x-hidden`}>
+        <Navbar/>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
