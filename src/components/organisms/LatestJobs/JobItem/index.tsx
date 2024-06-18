9a56965a9dbf46a18c027f7e9af9a574f08f403b
Mufid's Server <muhamadmufidbachri27@gmail.com>
2024-06-18 14:19:46 +0700

    commit

--- src/components/organisms/LatestJobs/JobItem/index.tsx
+++ src/components/organisms/LatestJobs/JobItem/index.tsx
@@ -4,48 +4,48 @@ import Image from "next/image";
 import React, { FC } from "react";
 
 interface JobItemProps {
-	image:string,
-	jobType:string,
-	location:string,
-	name:string,
-	type:string,
-	skills: string[],
+  image: string;
+  jobType: string;
+  location: string;
+  name: string;
+  type: string;
+  skills: string[];
 }
 
 const JobItem: FC<JobItemProps> = ({
-	image,
-	jobType,
-	location,
-	name,
-	type,
-	skills,
+  image,
+  jobType,
+  location,
+  name,
+  type,
+  skills,
 }) => {
-	return (
-		<div className="border border-border p-8 flex flex-row items-start gap-6 cursor-pointer">
-			<div>
-				<Image src={image} alt={image} width={64} height={64} />
-			</div>
-			<div>
-				<div className="text-lg font-semibold">{name}</div>
-				<div className="text-sm text-muted-foreground mb-2">
-					{type} . {location}
-				</div>
-				<div className="h-5 inline-flex gap-2 items-center">
-					<Badge variant="secondary">{jobType}</Badge>
-					<Separator orientation="vertical" />
-					{skills.slice(0, 2).map((item: string, i: number) => (
-						<Badge
-							key={item + i}
-							variant="outline"
-							className="rounded border-primary bg-primary/5 text-primary"
-						>
-							{item}
-						</Badge>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="border border-border p-8 flex flex-row items-start gap-6 cursor-pointer">
+      <div>
+        <Image src={image} alt={image} width={64} height={64} />
+      </div>
+      <div>
+        <div className="text-lg font-semibold">{name}</div>
+        <div className="text-sm text-muted-foreground mb-2">
+          {type} . {location}
+        </div>
+        <div className="h-5 inline-flex gap-2 items-center">
+          <Badge variant="secondary">{jobType}</Badge>
+          <Separator orientation="vertical" />
+          {skills.slice(0, 2).map((item: string, i: number) => (
+            <Badge
+              key={item + i}
+              variant="outline"
+              className="rounded border-primary bg-primary/5 text-primary"
+            >
+              {item}
+            </Badge>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default JobItem;
--- src/components/organisms/LatestJobs/index.tsx
+++ src/components/organisms/LatestJobs/index.tsx
@@ -1,5 +1,4 @@
-"use client"
-
+"use client";
 
 import TitleSection from "@/components/atoms/TitleSection";
 import React, { FC } from "react";
@@ -7,25 +6,25 @@ import JobItem from "./JobItem";
 interface LatestJobsProps {}
 
 const LatestJobs: FC<LatestJobsProps> = ({}) => {
+  return (
+    <div className="py-16 mt-32 mb-10 relative">
+      <TitleSection word1="Latest" word2="jobs open" />
 
-	return (
-		<div className="py-16 mt-32 mb-10 relative">
-			<TitleSection word1="Latest" word2="jobs open" />
-
-			<div className="mt-12 grid grid-cols-3 gap-8">
-				{[0,1,2].map((item: number) => (
-					<JobItem key={item}
-					image="/images/company.png"
-					jobType="Full Time"
-					location="Paris, France"
-					name="Email Marketing"
-					type="Agency"
-					skills={["Marketing", "Design"]}
-					/>
-				))}
-			</div>
-		</div>
-	);
+      <div className="mt-12 grid grid-cols-3 gap-8">
+        {[0, 1, 2].map((item: number) => (
+          <JobItem
+            key={item}
+            image="/images/company2.png"
+            jobType="Full Time"
+            location="Paris, France"
+            name="Email Marketing"
+            type="Agency"
+            skills={["Marketing", "Design"]}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LatestJobs;
